9e923667260b1b5016fc44167a5fb2d9b94e5907
Caminha <gabrielcaminha255@gmail.com>
2022-04-26 21:26:49 -0300

    requisito 4 pronto e css adicionado para a Home e lista de categorias

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.callCategoriesList = this.callCategoriesList.bind(this);
+
+    this.state = {
+      loading: false,
+      categoriesList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.callCategoriesList();
+  }
+
+  async callCategoriesList() {
+    this.setState({ loading: true });
+    const categoriesList = await getCategories();
+    this.setState(
+      { categoriesList },
+      this.setState({
+        loading: false,
+      }),
+    );
+  }
+
   render() {
+    const { loading, categoriesList } = this.state;
     return (
-      <div>
+      <div className="HomeDiv">
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
+        {!loading && (
+          <nav className="CategorySideBar">
+            {categoriesList.map((category) => (
+              <button
+                type="button"
+                data-testid="category"
+                key={ `${category.name}Button` }
+              >
+                {category.name}
+              </button>
+            ))}
+          </nav>
+        )}
       </div>
     );
   }
