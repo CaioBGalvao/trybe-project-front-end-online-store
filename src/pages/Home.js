fa69456de62b7faea9cdf053372e42d9546c59a0
Caminha <gabrielcaminha255@gmail.com>
2022-04-27 16:20:26 -0300

    componente CategoryList criado, Home limpa

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,53 +1,11 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
+import { CategoryList } from '../components/CategoryList';
 
 class Home extends Component {
-  constructor() {
-    super();
-
-    this.callCategoriesList = this.callCategoriesList.bind(this);
-
-    this.state = {
-      loading: false,
-      categoriesList: [],
-    };
-  }
-
-  componentDidMount() {
-    this.callCategoriesList();
-  }
-
-  async callCategoriesList() {
-    this.setState({ loading: true });
-    const categoriesList = await getCategories();
-    this.setState(
-      { categoriesList },
-      this.setState({
-        loading: false,
-      }),
-    );
-  }
-
   render() {
-    const { loading, categoriesList } = this.state;
     return (
       <div className="HomeDiv">
-        <h1 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
-        {!loading && (
-          <nav className="CategorySideBar">
-            {categoriesList.map((category) => (
-              <button
-                type="button"
-                data-testid="category"
-                key={ `${category.name}Button` }
-              >
-                {category.name}
-              </button>
-            ))}
-          </nav>
-        )}
+        <CategoryList />
       </div>
     );
   }
