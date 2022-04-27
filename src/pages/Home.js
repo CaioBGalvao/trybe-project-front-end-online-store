5e4fde43795cb5c5b845567405319f7072e431d6
Wellington <wellingtonmachioni@gmail.com>
2022-04-27 13:58:37 -0300

    Implementado requisito 5

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,81 @@
 import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import FormInput from '../components/FormInput';
+import ProductCard from '../components/Productcard';
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchKey: '',
+      resultSearch: [],
+      foundSomething: true,
+    };
+  }
+
+  setInputSearch = ({ target }) => {
+    const inputValue = target.value;
+    this.setState({ searchKey: inputValue });
+  }
+
+  searchProducts = async (e) => {
+    e.preventDefault();
+    const { searchKey } = this.state;
+    const { results } = await getProductsFromCategoryAndQuery(undefined, searchKey);
+    this.setState({
+      resultSearch: [...results],
+    }, () => {
+      const { resultSearch } = this.state;
+      if (resultSearch.length === 0) {
+        this.setState({ foundSomething: false });
+      } else {
+        this.setState({ foundSomething: true });
+      }
+    });
+  }
+
   render() {
+    const { searchKey, resultSearch, foundSomething } = this.state;
     return (
       <div>
+        <form onSubmit={ this.searchProducts }>
+          <FormInput
+            htmlFor="searchProducts"
+            type="text"
+            name="searchProducts"
+            placeHolder="Digite sua busca"
+            value={ searchKey }
+            onChange={ this.setInputSearch }
+            dataTestid="query-input"
+          />
+
+          <button
+            type="submit"
+            data-testid="query-button"
+          >
+            Pesquisar
+          </button>
+
+        </form>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
+        <section className="products">
+          {
+            foundSomething
+              ? resultSearch
+                .map((product) => (
+                  <ProductCard
+                    key={ product.id }
+                    title={ product.title }
+                    img={ product.thumbnail }
+                    price={ product.price }
+                  />
+                ))
+              : <p>Nenhum produto foi encontrado</p>
+          }
+        </section>
       </div>
     );
   }
