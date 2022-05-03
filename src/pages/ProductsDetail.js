0f36c676de905e36aa275090aa69619920aa537b
Wellington <wellingtonmachioni@gmail.com>
2022-05-03 16:43:48 -0300

    Implementado requisito 9

--- src/pages/ProductsDetail.js
+++ src/pages/ProductsDetail.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import CartButton from '../components/CartButton';
 
 export default class ProductsDetail extends Component {
+goCart = () => {
+  const { history: { push } } = this.props;
+  push('/cart');
+};
+
 goBack = () => {
   const { history: { push } } = this.props;
   push('/');
@@ -12,12 +18,34 @@ findProduct = () => {
   return resultSearch.find((whatINeedFound) => whatINeedFound.id === id);
 }
 
+handleQuantity = ({ target }) => {
+  const nameOfAction = target.name;
+  if (nameOfAction === 'quantity') {
+    const quantity = parseInt(target.value, 10);
+    this.setState({ productQuantity: quantity });
+  }
+
+  if (nameOfAction === 'plus') {
+    this.setState((prevState) => ({ productQuantity: prevState.productQuantity + 1 }));
+  }
+
+  if (nameOfAction === 'minus') {
+    this.setState((prevState) => ({ productQuantity: prevState.productQuantity - 1 }));
+  }
+}
+
+addToCart = () => {
+  const { addToCart, match: { params: { id } } } = this.props;
+  addToCart(id);
+}
+
 render() {
   const product = this.findProduct();
   return (
     <>
       <div>
         <button type="button" onClick={ this.goBack }>Voltar</button>
+        <CartButton goCart={ this.goCart } />
       </div>
       <div>
         <h1>{`${product.title} - R$ ${product.price}`}</h1>
@@ -30,6 +58,13 @@ render() {
           <li>Especificação 3</li>
         </ul>
 
+        <button
+          type="button"
+          onClick={ this.addToCart }
+          data-testid="product-detail-add-to-cart"
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     </>
   );
@@ -44,6 +79,7 @@ ProductsDetail.propTypes = {
       id: PropTypes.string.isRequired,
     }),
   }),
+  addToCart: PropTypes.func.isRequired,
 };
 
 ProductsDetail.defaultProps = {
